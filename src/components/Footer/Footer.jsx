4c9b3602745902dc23e147aec09ceb37f1574eb8
shotapailodze <shotikophailodze1998@gmail.com>
2023-10-26 14:58:19 +0400

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='top'>
+        <div className='item'>
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className='item'>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className='item'>
+          <h1>
+            About
+          </h1>
+          <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </span>
+        </div>
+        <div className='item'>
+          <h1>
+            Contact
+          </h1>
+          <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic</span>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='left'>
+          <span className="logo">PAILODZE</span>
+          <span className="copyright">
+          © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className='right'>
+          <img src='/public/img/paypal-logo.png' alt='logo'
+          
+           />
+        </div>
+      </div>
+    </div>
   )
 }
 
